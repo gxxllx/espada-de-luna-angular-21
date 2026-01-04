b0c5b0bbc699d70356ba8877882fd31e80b5402b
gxxllx <guillecom04@gmail.com>
2026-01-04 23:17:42 +0100

    Se mete variable de entorno con su tipo

---
+++ src/app/core/api/index.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Interfaz para la "envoltura" de la respuesta
+export interface ApiResponse<T> {
+  data: T;
+  status: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = NGX_ENV_.API_URL;
+
+  get<T>(endpoint: string): Observable<ApiResponse<T>> {
+    return this.http.get<ApiResponse<T>>(`${this.baseUrl}/${endpoint}`);
+  }
+
+  post<T, B = T>(endpoint: string, body: B): Observable<ApiResponse<T>> {
+    return this.http.post<ApiResponse<T>>(`${this.baseUrl}/${endpoint}`, body);
+  }
+
+  patch<T>(endpoint: string, body: Partial<T>): Observable<ApiResponse<T>> {
+    return this.http.patch<ApiResponse<T>>(`${this.baseUrl}/${endpoint}`, body);
+  }
+
+  delete(endpoint: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${endpoint}`);
+  }
+}
