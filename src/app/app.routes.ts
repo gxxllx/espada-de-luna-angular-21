259ffabd4ecfa03a110751467dd529d6a613090d
gxxllx <guillecom04@gmail.com>
2026-01-08 15:49:56 +0100

    RUTAS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,24 @@ import { Layout } from './layout/layout';
 export const routes: Routes = [
   {
     path: '',
-    component: Layout,
+    loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
