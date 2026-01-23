6785232eb6db2c3fa96b5f51204f7eac3db23974
gxxllx <guillecom04@gmail.com>
2026-01-23 17:29:14 +0100

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Layout } from './layout/layout';
 
 export const routes: Routes = [
   {
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { UserLogin } from '@/app/core/models/user.models';
+import { AuthService } from '@/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
 export class Login {
+  private readonly api = inject(AuthService);
+  private readonly fb = inject(FormBuilder);
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    user_password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const payload = this.loginForm.getRawValue() as UserLogin;
+
+      this.api.login(payload).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+        },
+        error: (err) => {
+          console.error('Error de login', err);
+        },
+      });
+    }
+  }
 }
