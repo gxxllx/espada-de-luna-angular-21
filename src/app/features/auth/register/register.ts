aac1d274231aba13add072efca84406ba76d51da
gxxllx <guillecom04@gmail.com>
2026-02-06 14:33:33 +0100

    Register a medio hacer con el control de contraseña

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Input } from '@/app/shared/components/input/input';
+import { Button } from '@/app/shared/components/button/button';
+import { UserRegister } from '@/app/core/models/user.models';
+import { AuthService } from '@/app/core/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, Input, Button],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  private readonly api = inject(AuthService);
+  private readonly fb = inject(FormBuilder);
 
+  passwordStrength = signal({ width: '1%', color: '#d73f40' });
+
+  registerForm = this.fb.group({
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    user_password: ['', Validators.required],
+    user_password_confirm: ['', Validators.required],
+  });
+
+  constructor() {
+    this.registerForm.get('user_password')?.valueChanges.subscribe((value) => {
+      this.checkPasswordStrength(value || '');
+    });
+  }
+
+  checkPasswordStrength(value: string) {
+    let point = 0;
+    const widthPower = ['1%', '25%', '50%', '75%', '100%'];
+    const colorPower = ['#d73f40', '#dc6551', '#f2b84f', '#bde952', '#3ba62f'];
+
+    if (value.length >= 6) {
+      const arrayTest = [/\d/, /[a-z]/, /[A-Z]/, /\W/];
+      arrayTest.forEach((item) => {
+        if (item.test(value)) {
+          point += 1;
+        }
+      });
+    }
+
+    this.passwordStrength.set({
+      width: widthPower[point],
+      color: colorPower[point],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const payload = this.registerForm.getRawValue() as UserRegister;
+
+      this.api.register(payload).subscribe({
+        next: (response) => {
+          console.log('Register successful:', response);
+        },
+        error: (err) => {
+          console.error('Register error:', err);
+        },
+      });
+    }
+  }
 }
