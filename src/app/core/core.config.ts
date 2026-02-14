f20baf33c9d910b6fb6c8c4b35247f919d7a90ef
gxxllx <guillecom04@gmail.com>
2026-02-14 19:13:39 +0100

    Se quita el SSR y ahora lo que hace es mirar la plataforma en la que se ejecuta angular y si esta plataforma es el navegador, que chequee la sesión (para ver si tenemos la cookie al recargar)

--- src/app/core/core.config.ts
+++ src/app/core/core.config.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { inject, provideAppInitializer } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID, provideAppInitializer } from '@angular/core';
 import { translateProviders } from './i18n/translate.config';
 import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 import { AuthService } from './services/auth.service';
@@ -10,7 +11,12 @@ export const CORE_CONFIG = [
   provideHttpClient(withInterceptors([credentialsInterceptor]), withFetch()),
   provideAppInitializer(() => {
     const authService = inject(AuthService);
-    return firstValueFrom(authService.checkSession());
+    const platformId = inject(PLATFORM_ID);
+
+    if (isPlatformBrowser(platformId)) {
+      return firstValueFrom(authService.checkSession());
+    }
+    return Promise.resolve();
   }),
   // Guards and other core providers can be added here
 ];
