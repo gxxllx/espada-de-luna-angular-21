368bfbdbb3e316cf138c911312aca73ef6ddd44b
gxxllx <guillecom04@gmail.com>
2026-02-14 19:53:37 +0100

    Al final la aplicación de angular se puede separar en 2, una de admin y otra de usuario así que podemos hacer la app "Monolítica"

--- src/environments/environment.admin.ts
+++ src/environments/environment.admin.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: _NGX_ENV_.NODE_ENV === 'production',
   apiUrl: _NGX_ENV_.NG_APP_API_URL,
-  isAdmin: false,
+  isAdmin: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: _NGX_ENV_.NODE_ENV === 'development',
   apiUrl: _NGX_ENV_.NG_APP_API_URL,
   googleClientId: '',
+  isAdmin: false,
 };
