01d3934425b56b5d8767eac68407edd81111a8be
gxxllx <guillecom04@gmail.com>
2026-02-19 16:40:51 +0100

    Se añade footer en el layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './layout.html',
   styleUrls: ['./layout.scss'],
 })
