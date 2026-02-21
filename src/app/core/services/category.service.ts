fdf0fd437542afe30e0a6371f6ecb55f61ea77f4
gxxllx <guillecom04@gmail.com>
2026-02-21 13:33:27 +0100

    Categorías ahora se cachea

--- src/app/core/services/category.service.ts
+++ src/app/core/services/category.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { ApiService, ApiResponse } from '../api';
+import { CacheService } from '../cache';
 import { ENDPOINTS } from '../constants/endpoints';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { Category } from '../models/category.models';
 
 @Injectable({
@@ -9,9 +10,24 @@ import { Category } from '../models/category.models';
 })
 export class CategoryService {
   private readonly api = inject(ApiService);
+  private readonly cache = inject(CacheService);
+  private readonly cacheKey = 'categories';
   private readonly routes = ENDPOINTS.CATEGORY;
 
   getAll(): Observable<ApiResponse<Category[]>> {
-    return this.api.get<Category[]>(`${this.routes.BASE}`);
+    const cached = this.cache.get<Category[]>(this.cacheKey);
+    if (cached) {
+      return of({ data: cached });
+    }
+
+    return this.api.get<Category[]>(`${this.routes.BASE}`).pipe(
+      tap((response) => {
+        this.cache.set(this.cacheKey, response.data);
+      }),
+    );
+  }
+
+  clearCache(): void {
+    this.cache.clear(this.cacheKey);
   }
 }
