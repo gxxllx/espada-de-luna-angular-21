131b5f1963cd0ddfd05460643af049f800ff7e5e
gxxllx <guillecom04@gmail.com>
2026-02-21 13:02:55 +0100

    Se omite el isLoggedIn para el guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,16 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (_route, _state) => {
-  const authService = inject(AuthService);
   const router = inject(Router);
 
   const hasToken = document.cookie
     .split(';')
     .some((cookie) => cookie.trim().startsWith('access_token='));
 
-  if (hasToken && authService.isLoggedIn()) {
+  if (hasToken) {
     return true;
   }
 
